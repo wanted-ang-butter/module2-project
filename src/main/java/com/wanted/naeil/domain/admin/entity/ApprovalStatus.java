3491d7b16770c1ca6964b9a1d3d451e528d08919
vinent-an <xxv211@naver.com>
2026-04-15 10:23:29 +0900

    승이 완료 -> 승인 완료

--- src/main/java/com/wanted/naeil/domain/admin/entity/ApprovalStatus.java
+++ src/main/java/com/wanted/naeil/domain/admin/entity/ApprovalStatus.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ApprovalStatus {
     PENDING("승인 대기"),
-    APPROVED("승이 완료"),
+    APPROVED("승인 완료"),
     REJECTED("반려");
 
     private final String description;
