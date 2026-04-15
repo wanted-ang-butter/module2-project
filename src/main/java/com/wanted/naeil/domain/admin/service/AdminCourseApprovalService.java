be1f113d093fdf4211ce1506d57be260fb6a6ce9
vincent-an <78752964+vincent-an@users.noreply.github.com>
2026-04-15 17:14:11 +0900

    devlop 파일 통합을 위한 코드 수정
    devlop 파일 통합을 위한 코드 수정

--- src/main/java/com/wanted/naeil/domain/admin/service/AdminCourseApprovalService.java
+++ src/main/java/com/wanted/naeil/domain/admin/service/AdminCourseApprovalService.java
@@ -1,19 +1,19 @@
-//package com.wanted.naeil.domain.admin.service;
-//
-//import com.wanted.naeil.domain.admin.dto.response.CourseApprovalResponse;
-//import com.wanted.naeil.domain.admin.entity.ApprovalRequestType;
-//import com.wanted.naeil.domain.admin.entity.ApprovalStatus;
-//import com.wanted.naeil.domain.admin.entity.CourseApproval;
-//import com.wanted.naeil.domain.admin.repository.CourseApprovalRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//@Service   // 스프링한테 서비스 클래스라고 알려줘야쥐잉
-//@RequiredArgsConstructor //service에서 Repository 쓰야하니까 필드선언
-//public class AdminCourseApprovalService {
+package com.wanted.naeil.domain.admin.service;
+
+import com.wanted.naeil.domain.admin.dto.response.CourseApprovalResponse;
+import com.wanted.naeil.domain.admin.entity.ApprovalRequestType;
+import com.wanted.naeil.domain.admin.entity.ApprovalStatus;
+import com.wanted.naeil.domain.admin.entity.CourseApproval;
+import com.wanted.naeil.domain.admin.repository.CourseApprovalRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service   // 스프링한테 서비스 클래스라고 알려줘야쥐잉
+@RequiredArgsConstructor //service에서 Repository 쓰야하니까 필드선언
+public class AdminCourseApprovalService {
 //    private final CourseApprovalRepository courseApprovalRepository;
 //    // @RequiredArgsConstructor 있으면 final 필드보고 원래
 //    //public AdminCourseApprovalService(CourseApprovalRepository courseApprovalRepository) {
@@ -29,4 +29,4 @@
 //                .collect(Collectors.toList());
 //
 //    }
-//}
+}
