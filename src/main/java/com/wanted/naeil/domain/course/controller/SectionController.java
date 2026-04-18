2686c5eac953fec4b5a459680d4b2e87905e6781
vinent-an <xxv211@naver.com>
2026-04-19 04:40:38 +0900

    코스 수정, 섹션 수정, 섹션 추가, 섹션 삭제 기능 구현 - 이슈 120~123 참고

--- src/main/java/com/wanted/naeil/domain/course/controller/SectionController.java
+++ src/main/java/com/wanted/naeil/domain/course/controller/SectionController.java
@@ -1,27 +1,30 @@
 package com.wanted.naeil.domain.course.controller;
 
 
+import com.wanted.naeil.domain.course.dto.request.SectionUpdateRequest;
+import com.wanted.naeil.domain.course.dto.request.UploadSectionRequest;
 import com.wanted.naeil.domain.course.dto.response.SectionStudyResponse;
 import com.wanted.naeil.domain.course.service.SectionService;
 import com.wanted.naeil.global.auth.model.dto.AuthDetails;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/course")
 @RequiredArgsConstructor
 @Slf4j
 public class SectionController {
 
     private final SectionService sectionService;
 
-    @GetMapping("/{courseId}/sections/{sectionId}")
+    // 섹션 상세 조회 (섹션 수강)
+    @GetMapping("/course/{courseId}/sections/{sectionId}")
     public ModelAndView getSectionStudyPage(
             @AuthenticationPrincipal AuthDetails authDetails,
             @PathVariable Long courseId, @PathVariable Long sectionId,
@@ -36,5 +39,58 @@ public class SectionController {
         mv.setViewName("course/sectionDetail");
 
         return mv;
+    }
+
+    // 섹션 수정 - 강사
+    @PatchMapping(value = "/instructor/course/{courseId}/sections/{sectionId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    public ResponseEntity<Void> updateSection(
+            @AuthenticationPrincipal AuthDetails authDetails,
+            @PathVariable Long courseId,
+            @PathVariable Long sectionId,
+            @Valid @ModelAttribute SectionUpdateRequest request
+    ) {
+        Long instructorId = authDetails.getLoginUserDTO().getUserId();
+
+        log.info("[SectionUpdate] 섹션 수정 요청 - instructorId: {}, courseId: {}, sectionId: {}",
+                instructorId, courseId, sectionId);
+
+        sectionService.updateSection(instructorId, courseId, sectionId, request);
+        return ResponseEntity.ok().build();
+    }
+
+    // 섹션 추가 - 강사
+    @PostMapping("/instructor/course/{courseId}/sections")
+    @ResponseBody
+    public ResponseEntity<Void> addSection(
+            @AuthenticationPrincipal AuthDetails authDetails,
+            @PathVariable Long courseId,
+            @Valid @ModelAttribute UploadSectionRequest request
+    ) {
+        Long instructorId = authDetails.getLoginUserDTO().getUserId();
+
+        log.info("[SectionCreate] 섹션 추가 요청 - instructorId: {}, courseId: {}", instructorId, courseId);
+
+        sectionService.createSection(instructorId, courseId, request);
+
+        return ResponseEntity.ok().build();
+    }
+
+    // 섹션 삭제 - 강사
+    @DeleteMapping("/instructor/course/{courseId}/sections/{sectionId}")
+    @ResponseBody
+    public ResponseEntity<Void> deleteSection(
+            @AuthenticationPrincipal AuthDetails authDetails,
+            @PathVariable Long courseId,
+            @PathVariable Long sectionId
+    ) {
+        Long instructorId = authDetails.getLoginUserDTO().getUserId();
+
+        log.info("[SectionDelete] 섹션 삭제 요청 - instructorId: {}, courseId: {}, sectionId: {}",
+                instructorId, courseId, sectionId);
+
+        sectionService.deleteSection(instructorId, courseId, sectionId);
+
+        return ResponseEntity.ok().build();
     }
 }
