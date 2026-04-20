bf1d3571865040d99bd967aeb42447a0af19f9b4
030clin <030clin@gmail.com>
2026-04-20 21:51:10 +0900

    [feature] #177 #178 #179

--- src/main/java/com/wanted/naeil/domain/settlement/entity/enums/SettlementStatus.java
+++ src/main/java/com/wanted/naeil/domain/settlement/entity/enums/SettlementStatus.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum SettlementStatus {
 
-    PENDING("승인 대기"),
+    READY("신청 가능"),
+    PENDING("승인 대기"), // 강사 신청 완료, 관리자 처리 대기
     APPROVED("승인 완료"),
     REJECTED("반려"),
     CANCELED("강사 취소");
